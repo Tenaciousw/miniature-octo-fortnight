36138e994bc01af557f7e751318985842ea27f02
Nicolas <njw@live.com.ar>
2022-03-16 08:42:46 -0300

    Cambie por Bart

--- src/index.ts
+++ src/index.ts
@@ -6,23 +6,52 @@ const app = new Application({
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
 	backgroundColor: 0x6495ed,
-	width: 500,
-	height: 344
+	width: 720,
+	height: 720
 });
 
-Loader.shared.add({url: "./kramer.jpg", name: "Cosmo"});
+window.addEventListener("resize", ()=>{
+	const scaleX = window.innerWidth / app.screen.width;
+	const scaleY = window.innerHeight / app.screen.height;
+	const scale = Math.min(scaleX, scaleY);
+
+	const gameWidth = Math.round(app.screen.width * scale);
+	const gameHeight = Math.round(app.screen.height * scale);
+
+	const marginHorizontal = Math.floor((window.innerWidth - gameWidth) / 2);
+	const marginVertical = Math.floor((window.innerHeight - gameHeight) / 2);
+
+	app.view.style.width = gameWidth + "px";
+	app.view.style.height = gameHeight + "px";
+
+	app.view.style.marginLeft = marginHorizontal + "px";
+	app.view.style.marginRight = marginHorizontal + "px";
+
+	app.view.style.marginTop = marginVertical + "px";
+	app.view.style.marginBottom = marginVertical + "px";
+});
+window.dispatchEvent(new Event("resize"));
+
+Loader.shared.add({url: "./bart.png", name: "Bart"});
 
 Loader.shared.onComplete.add(()=>{
 
-const kramer: Sprite = Sprite.from("Cosmo");
+const Bart: Sprite = Sprite.from("Bart");
 
-console.log("Hola Mundo!",kramer.width,kramer.height);
+console.log("Multiplicate por cero!",Bart.width,Bart.height);
 
-kramer.x = 0;
-kramer.y = 0;
+Bart.x = 0;
+Bart.y = 0;
 
-app.stage.addChild(kramer);
+Bart.scale.x =0.5;
+Bart.scale.y =0.5;
+
+Bart.angle = -45;
+
+app.stage.addChild(Bart);
 
 });
 
 Loader.shared.load();
+
+
